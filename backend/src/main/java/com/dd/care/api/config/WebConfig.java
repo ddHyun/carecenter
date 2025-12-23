f19606531133261ce64ff634a40fb57c2073231f
kshyun <kshyun@taihoinst.com>
2025-12-23 16:12:51 +0900

    CORS 설정 추가

--- backend/src/main/java/com/dd/care/api/config/WebConfig.java
+++ backend/src/main/java/com/dd/care/api/config/WebConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*");
     }
 }
